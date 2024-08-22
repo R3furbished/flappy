94fcc0d626d1baf95dae237a7512fb6038b33e30
R3furbished <migas.2.3@outlook.pt>
2024-08-22 22:02:35 +0100

    Base game is done , still want to make movement smoother and obstacles to apear quicker.

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,9 @@ use bracket_lib::prelude::*;
 struct State {
     player: Player,
     frame_time: f32,
+    obstacle: Obstacle,
     game_mode: GameMode,
+    score: i32,
 }
 
 impl State {
@@ -11,7 +13,9 @@ impl State {
         State {
             player: Player::new(5, 25),
             frame_time: 0.0,
+            obstacle: Obstacle::new(SCREEN_WIDTH, 0),
             game_mode: GameMode::Menu,
+            score: 0,
         }
     }
 
@@ -42,16 +46,26 @@ impl State {
             self.player.flap();
         }
 
+        self.obstacle.render(contex, self.player.x);
         self.player.render(contex);
-        contex.print(0, 0, "Press SPACE to flap.");
 
-        if self.player.y > SCREEN_HEIGHT {
+        if self.player.x > self.obstacle.x {
+            self.score += 1;
+            self.obstacle = Obstacle::new(self.player.x + SCREEN_WIDTH, self.score);
+        }
+
+        contex.print(0, 0, "Press SPACE to flap.");
+        contex.print(0, 1, &format!("Score: {}", self.score));
+
+        if self.player.y > SCREEN_HEIGHT || self.obstacle.hit_obstacle(&self.player) {
             self.game_mode = GameMode::End;
         }
     }
 
     fn restart(&mut self) {
         self.player = Player::new(5, 25);
+        self.score = 0;
+        self.obstacle = Obstacle::new(SCREEN_WIDTH, 0);
         self.frame_time = 0.0;
         self.game_mode = GameMode::Playing;
     }
@@ -59,6 +73,7 @@ impl State {
     fn lost(&mut self, contex: &mut BTerm) {
         contex.cls();
         contex.print_centered(5, "You lost!");
+        contex.print_centered(6, &format!("You got {} points", self.score));
         contex.print_centered(8, "(P) Play Game");
         contex.print_centered(9, "(Q) Quit Game");
 
@@ -88,6 +103,49 @@ struct Player {
     velocity: f32,
 }
 
+struct Obstacle {
+    x: i32,
+    gap_y: i32,
+    size: i32,
+}
+
+impl Obstacle {
+    fn new(x: i32, score: i32) -> Obstacle {
+        let mut random = RandomNumberGenerator::new();
+        Obstacle {
+            x,
+            gap_y: random.range(10, 40), //Defines epicenter to expand gap
+            size: i32::max(2, 20 - score), //By doing this the gap will shrink the higher the
+                                         //player score is but never less than 2;
+        }
+    }
+
+    fn hit_obstacle(&self, player: &Player) -> bool {
+        let half_size = self.size / 2;
+        let does_x_match = player.x == self.x;
+        let player_above_gap = player.y < self.gap_y - half_size;
+        let player_below_gap = player.y > self.gap_y + half_size;
+
+        return does_x_match && (player_above_gap || player_below_gap);
+    }
+
+    fn render(&mut self, context: &mut BTerm, player_x: i32) {
+        // By subtracting the player x from
+        let screen_x = self.x - player_x; //translating from world-space to screen-space;
+        let half_size = self.size / 2;
+
+        //Gap being the center we need to render top from 0 to gap - size/2 and bottom from gap +
+        //size/2;
+        for y in 0..self.gap_y - half_size {
+            context.set(screen_x, y, RED, BLACK, to_cp437('|'));
+        }
+
+        for y in self.gap_y + half_size..SCREEN_HEIGHT {
+            context.set(screen_x, y, RED, BLACK, to_cp437('|'));
+        }
+    }
+}
+
 impl Player {
     fn new(x: i32, y: i32) -> Player {
         Player {
@@ -103,7 +161,7 @@ impl Player {
 
     fn gravity_and_move(&mut self) {
         if self.velocity < 2.0 {
-            self.velocity += 0.5;
+            self.velocity += 0.2;
         }
         self.y += self.velocity as i32;
         self.x += 1;
